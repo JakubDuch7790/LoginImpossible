39ea031a6ff2b0317360aaffb8080f17d4e1951a
Jakub Duch <jakub.duch23@gmail.com>
2023-12-01 23:11:03 +0100

    Ignore second try

--- LoginImpossible/Program.cs
+++ LoginImpossible/Program.cs
@@ -2,6 +2,7 @@ namespace LoginImpossible
 {
     public class Program
     {
+        //fake testing commit
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
